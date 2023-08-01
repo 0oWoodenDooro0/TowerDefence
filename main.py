0feec7e58e112268631f05e0fbc3aee41a968705
0oWoodenDooro0 <vincent031525@gmail.com>
2023-08-01 10:25:34 +0800

    添加Enemy,路徑

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import constants as c
 import pygame as pg
+from enemy import Enemy
 
 pg.init()
 
@@ -8,13 +9,38 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption(c.TITLE)
 
+waypoints = [
+    (100, 100),
+    (400, 200),
+    (400, 100),
+    (200, 300)
+]
+
+enemy_image = pg.image.load('assets/enemy/enemy1.png').convert_alpha()
+
+enemy_group = pg.sprite.Group()
+
+enemy = Enemy(waypoints, enemy_image)
+enemy_group.add(enemy)
+
+
 run = True
 while run:
 
     clock.tick(c.FPS)
 
+    screen.fill("grey100")
+
+    pg.draw.lines(screen, "grey0", False, waypoints)
+
+    enemy_group.update()
+
+    enemy_group.draw(screen)
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+
+    pg.display.flip()
 
 pg.quit()
