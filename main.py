a128090e9cbc984e6b9e4a38f2b66c5ff5275c13
0oWoodenDooro0 <vincent031525@gmail.com>
2023-08-02 11:08:03 +0800

    新增按鈕點及創建tower並且在進入tower射程範圍內會鎖定敵人

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import constants as c
 from enemy import Enemy
 from tower import Tower
 from world import World
+from button import Button
 
 pg.init()
 
@@ -11,6 +12,9 @@ clock = pg.time.Clock()
 
 screen = pg.display.set_mode((c.SCREEN_WIDTH + c.SIDE_PANEL, c.SCREEN_HEIGHT))
 pg.display.set_caption(c.TITLE)
+
+placing_tower = False
+selected_tower = None
 
 map_image = pg.image.load('assets/level/level1.png').convert_alpha()
 
@@ -36,6 +40,19 @@ def create_tower(mouse_pos):
             tower_group.add(new_tower)
 
 
+def select_tower(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // c.TILE_SIZE
+    for tower in tower_group:
+        if (mouse_tile_x, mouse_tile_y) == (tower.tile_x, tower.tile_y):
+            return tower
+
+
+def clear_selection():
+    for tower in tower_group:
+        tower.selected = False
+
+
 world = World(map_image)
 
 enemy_group = pg.sprite.Group()
@@ -45,26 +62,51 @@ enemy = Enemy(c.WAYPOINTS, enemy_image)
 
 enemy_group.add(enemy)
 
+tower_button = Button(c.SCREEN_WIDTH + 30, 120, buy_tower_image, True)
+cancel_button = Button(c.SCREEN_WIDTH + 30, 180, cancel_image, True)
+
 run = True
 while run:
 
     clock.tick(c.FPS)
-
+    # Updating
     screen.fill("grey100")
 
+    enemy_group.update()
+    tower_group.update(enemy_group)
+
+    if selected_tower:
+        selected_tower.selected = True
+    # Drawing
     world.draw(screen)
 
-    enemy_group.update()
-
     enemy_group.draw(screen)
-    tower_group.draw(screen)
+    for tower in tower_group:
+        tower.draw(screen)
 
+    if tower_button.draw(screen):
+        placing_tower = True
+    if placing_tower:
+        cursor_rect = cursor_tower.get_rect()
+        cursor_pos = pg.mouse.get_pos()
+        cursor_rect.center = cursor_pos
+        if cursor_pos[0] <= c.SCREEN_WIDTH:
+            screen.blit(cursor_tower, cursor_rect)
+        if cancel_button.draw(screen):
+            placing_tower = False
+    # event
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             mouse_pos = pg.mouse.get_pos()
-            create_tower(mouse_pos)
+            if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+                selected_tower = None
+                clear_selection()
+                if placing_tower:
+                    create_tower(mouse_pos)
+                else:
+                    selected_tower = select_tower(mouse_pos)
 
     pg.display.flip()
 
--- tower.py
+++ tower.py
@@ -1,15 +1,57 @@
 import pygame as pg
 import constants as c
+import math
 
 
 class Tower(pg.sprite.Sprite):
     def __init__(self, image, tile_x, tile_y):
         pg.sprite.Sprite.__init__(self)
+        self.target = None
+        self.range = 180
+        self.cooldown = 1500
+        self.last_shot = pg.time.get_ticks()
+        self.selected = False
+
         self.tile_x = tile_x
         self.tile_y = tile_y
-
         self.x = (self.tile_x + 0.5) * c.TILE_SIZE
         self.y = (self.tile_y + 0.5) * c.TILE_SIZE
-        self.image = image
+
+        self.angle = 90
+        self.original_image = image
+        self.image = pg.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
+
+        self.range_image = pg.Surface((self.range * 2, self.range * 2))
+        self.range_image.fill((0, 0, 0))
+        self.range_image.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.range_image, "grey100", (self.range, self.range), self.range)
+        self.range_image.set_alpha(100)
+        self.range_rect = self.range_image.get_rect()
+        self.range_rect.center = self.rect.center
+
+    def update(self, enemy_group):
+        if self.target:
+            self.last_shot = pg.time.get_ticks()
+            self.target = None
+        else:
+            if pg.time.get_ticks() - self.last_shot > self.cooldown:
+                self.pick_target(enemy_group)
+
+    def pick_target(self, enemy_group):
+        for enemy in enemy_group:
+            x_dist = enemy.pos[0] - self.x
+            y_dist = enemy.pos[1] - self.y
+            dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
+            if dist < self.range:
+                self.target = enemy
+                self.angle = math.degrees(math.atan2(-y_dist, x_dist))
+
+    def draw(self, surface):
+        self.image = pg.transform.rotate(self.original_image, self.angle - 90)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        surface.blit(self.image, self.rect)
+        if self.selected:
+            surface.blit(self.range_image, self.range_rect)
